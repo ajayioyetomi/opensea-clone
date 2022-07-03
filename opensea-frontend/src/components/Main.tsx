a024d44cd150c2c842345dd1de4770eb711f82bb
ajayioyetomi <ajayi.teea@yahoo.com>
2022-07-03 20:53:54 +0100

    added notable drops

--- opensea-frontend/src/components/Main.tsx
+++ opensea-frontend/src/components/Main.tsx
@@ -1,7 +1,7 @@
 import css from './css/main.module.css';
 import bannerImage from '../images/peripheral-image.jpg';
 import doopIcon from '../images/worldofwoman.png';
-import {MdPlayCircle as PlayIcon} from 'react-icons/md';
+import {MdPlayCircle as PlayIcon,MdKeyboardArrowLeft as ArrowLeft,MdKeyboardArrowRight as ArrowRight} from 'react-icons/md';
 
 const Main = () => {
   return (
@@ -38,9 +38,19 @@ const Main = () => {
         </section>
         <section className={css.notableSection}>
             <h1>Notable Drops</h1>
-            <div className={css.slideContainer}>
-                <div></div>
-                <div></div>
+            <div className={css.notableSlideContainer}>
+                <div className={css.slideContainer}>
+                  <div></div>
+                  <div></div>
+                  <div></div>
+                  <div></div>
+                  <div></div>
+                </div>
+                <div className={css.slideDirection}>
+                  <span><ArrowLeft /></span>
+                  <span><ArrowRight /></span>
+                </div>
+                <div className={css.slideIndicator}></div>
             </div>
         </section>
     </main>
